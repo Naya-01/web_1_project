d20894ed5b7d3134b83ca36cdce24d1942173180
François Bardijn <francois.bardijn@student.vinci.be>
2021-04-30 10:07:36 +0200

    Correction du problème de changement de statuts

--- views/post_handling.php
+++ views/post_handling.php
@@ -33,10 +33,12 @@
                     <form class="buttons are-medium" action="index.php?action=post_handling" method="post">
                         <input type="hidden" name="post-handling-id" value="<?php echo $idea->html_id_idea()?>">
                         <?php if ($idea->status() != 'C') { ?>
-                            <input class="button is-danger is-light is-small" name="closed" type="submit" value="Fermer">
-                        <?php } if ($idea->status() == 'T') { ?>
-                            <input class="button is-danger is-light is-small" name="refused" type="submit" value="Refuser">
-                            <input class="button is-danger is-light is-small" name="accepted" type="submit" value="Accepter">
+                                <input class="button is-danger is-light is-small" name="closed" type="submit" value="Fermer">
+                            <?php if ($idea->status() != 'R') { ?>
+                                <input class="button is-danger is-light is-small" name="refused" type="submit" value="Refuser">
+                            <?php } if ($idea->status() != 'A') { ?>
+                                <input class="button is-danger is-light is-small" name="accepted" type="submit" value="Accepter">
+                            <?php } ?>
                         <?php } ?>
                     </form>
                 </div>
